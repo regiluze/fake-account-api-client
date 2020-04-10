1f4fa792ae91b6f7446883dd2049f5e58ca3d2aa
regiluze <regiluze@hotmail.com>
2020-04-11 01:38:23 +0200

    Add Do method to httpClient interface, it the same signature as go library http Client Do method

--- client.go
+++ client.go
@@ -1,12 +1,17 @@
 package accountclient
 
-func NewForm3Client(httpClient HTTPClient) *Form3Client {
-	return &Form3Client{}
-}
+import (
+	"net/http"
+)
 
 type HTTPClient interface {
+	Do(req *http.Request) (*http.Response, error)
 }
 
 type Form3Client struct {
 	httpClient HTTPClient
+}
+
+func NewForm3Client(httpClient HTTPClient) *Form3Client {
+	return &Form3Client{httpClient}
 }
--- client_test.go
+++ client_test.go
@@ -2,24 +2,27 @@ package accountclient
 
 import (
 	"fmt"
+	gomock "github.com/golang/mock/gomock"
 	. "github.com/onsi/ginkgo"
 	//. "github.com/onsi/gomega"
 )
 
 var _ = Describe("Account api resource client", func() {
 	var (
-		client *Form3Client
-		//httpClientMock HTTPClientMock
+		client         *Form3Client
+		mockCtrl       *gomock.Controller
+		httpClientMock = NewMockHTTPClient(mockCtrl)
 	)
 
 	BeforeEach(func() {
-		client = NewForm3Client()
+		client = NewForm3Client(httpClientMock)
 	})
 
 	Describe("Resource operations", func() {
 		Context("Create", func() {
 			It("blablabla", func() {
 				fmt.Println(">>>> client ", client)
+				//httpClient.Mock.Expecs
 			})
 		})
 	})
