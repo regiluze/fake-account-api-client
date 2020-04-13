66946111ef3e9cb28740d48a5b129d7b4cf559a3
regiluze <regiluze@hotmail.com>
2020-04-13 23:52:13 +0200

    Trace bullet to interact with the api, creating a new account and getting the valid response

--- client.go
+++ client.go
@@ -1,7 +1,13 @@
 package accountclient
 
 import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
 	"net/http"
+
+	"./resources"
 )
 
 type HTTPClient interface {
@@ -9,9 +15,44 @@ type HTTPClient interface {
 }
 
 type Form3Client struct {
+	url        string
 	httpClient HTTPClient
 }
 
-func NewForm3Client(httpClient HTTPClient) *Form3Client {
-	return &Form3Client{httpClient}
+func NewForm3Client(apiBaseURL string, httpClient HTTPClient) *Form3Client {
+	return &Form3Client{apiBaseURL, httpClient}
+}
+
+type User struct {
+	ResourceType   string                 `json:"type"`
+	Id             string                 `json:"id"`
+	OrganisationId string                 `json:"organizarion_id"`
+	Attributes     map[string]interface{} `json:"attributes"`
+}
+
+func (fc Form3Client) Create(resource resources.Resource) (*resources.DataContainer, error) {
+	data := resources.NewDataContainer(resource)
+	dataBt, err := json.Marshal(data)
+	if err != nil {
+		return nil, err
+	}
+	req, _ := http.NewRequest(
+		"POST",
+		fc.url,
+		bytes.NewBuffer(dataBt),
+	)
+	req.Header.Set("Accept", "application/vnd.api+json")
+	req.Header.Set("Content-Type", "application/vnd.api+json")
+	resp, err := fc.httpClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+	fmt.Println(">>>> status code ", resp.StatusCode)
+	body, err := ioutil.ReadAll(resp.Body)
+	fmt.Println(">>>> resp body ", string(body))
+	var responseData resources.DataContainer
+	err = json.Unmarshal(body, &responseData)
+	fmt.Println(">>>> unmarshal error ", err)
+	return &responseData, nil
 }
--- client_test.go
+++ client_test.go
@@ -2,27 +2,50 @@ package accountclient
 
 import (
 	"fmt"
+	"net/http"
+
 	gomock "github.com/golang/mock/gomock"
 	. "github.com/onsi/ginkgo"
+
 	//. "github.com/onsi/gomega"
+	"./resources"
+)
+
+const (
+	id             = "ad27e265-4402-3b3b-a0e5-3004ea9cc8dc"
+	organisationId = "eb0bd6f5-c3f5-44b2-b677-acd23cdde73c"
 )
 
 var _ = Describe("Account api resource client", func() {
 	var (
 		client         *Form3Client
 		mockCtrl       *gomock.Controller
-		httpClientMock = NewMockHTTPClient(mockCtrl)
+		httpClientMock *MockHTTPClient
 	)
 
 	BeforeEach(func() {
-		client = NewForm3Client(httpClientMock)
+		mockCtrl = gomock.NewController(GinkgoT())
+		httpClientMock = NewMockHTTPClient(mockCtrl)
+		client = NewForm3Client("http://localhost:8080/v1/organisation/accounts", http.DefaultClient)
+		//client = NewForm3Client("http://localhost:8080/v1/organisation/accounts", httpClientMock)
 	})
 
 	Describe("Resource operations", func() {
 		Context("Create", func() {
 			It("blablabla", func() {
-				fmt.Println(">>>> client ", client)
-				//httpClient.Mock.Expecs
+				httpClientMock.EXPECT().Do(gomock.Any()).Return(nil, nil).Times(1)
+
+				attributes := map[string]interface{}{
+					"country":       "GB",
+					"base_currency": "GBP",
+					"bank_id":       "400300",
+					"bank_id_code":  "GBDSC",
+					"bic":           "NWBKGB22",
+				}
+				resource := resources.NewAccount(id, organisationId, attributes)
+
+				response, _ := client.Create(resource)
+				fmt.Printf(">>>>> response %+v", response)
 			})
 		})
 	})
