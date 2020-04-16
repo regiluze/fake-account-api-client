8e5a56c46f51e278b78bedf27a3eadd452696160
regiluze <regiluze@hotmail.com>
2020-04-17 00:29:32 +0200

    Add delete method e2e test

--- apiEnd2End_test.go
+++ apiEnd2End_test.go
@@ -12,8 +12,9 @@ import (
 )
 
 const (
-	id             = "ad67e265-4402-3b3b-a0e5-3004ea9cc8dc"
+	id             = "ad69e865-4402-3b3b-a0e5-3004ea9cc8dc"
 	organisationID = "eb0bd6f5-c3f5-44b2-b677-acd23cdde73c"
+	version        = 0
 	baseURL        = "http://localhost:8080/v1"
 )
 
@@ -54,6 +55,14 @@ var _ = Describe("", func() {
 				Expect(err).To(BeNil())
 				Expect(resp.Data.ID).To(Equal(id))
 				fmt.Println(">>>> response data ", resp)
+			})
+		})
+		Context("Delete", func() {
+			It("delete an account with provided 'id' parameter", func() {
+
+				err := apiClient.DeleteAccount(id, version)
+
+				Expect(err).To(BeNil())
 			})
 		})
 	})
